527fcc91b35f8fff9ed095b8d81eaa5f9698d78e
lugliob <biagio@redhedge.com>
2024-09-04 22:33:33 +0200

    infinite loop, timestamp, multi process

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::thread;
-
+use ctrlc;
+use chrono::Local;
 use lamport_vector_clocks::ThreadPool;
 fn main() {
-    let pool = ThreadPool::new(1);
+    let pool = ThreadPool::new(2);
     for _ in 0..1 {
         pool.execute(|| {
             run();
         });
     }
-    pool.send_message("this is a message".to_string());
-    println!("job and message sent");
-    thread::sleep(std::time::Duration::from_secs(4));
-    println!("done");
+
+    ctrlc::set_handler(move || {
+        std::process::exit(0);
+    }).expect("error setting Ctrl+c handler");
+
+    loop {
+        let now = Local::now();
+        let timestamp_msg = format!("{}", now);
+        pool.send_message(timestamp_msg);
+        thread::sleep(std::time::Duration::from_secs(3));
+    }
 }
 
 fn run(){
